d5486d16ab7963bf824520b4964b39200a77a5e4
Garcia <adem.okur5734@gmail.com>
2022-10-10 20:51:19 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*int sayi = 1;
+        boolean isPrime = true;
+
+        if (sayi == 1) {
+            System.out.println("Sayi Asal Degildir.");
+            return;
+        }
+        if (sayi < 1) {
+            System.out.println("Gecersiz Sayi Girdiniz.");
+            return;
+        }
+
+        for (int i = 2; i < sayi; i++) {
+            if (sayi % i == 0) {
+                isPrime = false;
+                System.out.println("Sayi Asal sayi degildr.");
+            }
+
+
+        }
+        if (isPrime) {
+            System.out.println("Sayi Asaldir.");
+        }*/
+
+
+
+       
+
+
+    }
+}
